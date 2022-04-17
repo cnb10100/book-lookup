a493ba10907f8675cb6cee1dc866663d20d4ecfd
Cysmi <cnb10100@gmail.com>
2022-04-16 20:48:04 -0400

    fixed function to save books

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -79,9 +79,13 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {
+          input: bookToSave,
+        },
+      });
 
-      if (!response.ok) {
+      if (!response) {
         throw new Error("something went wrong!");
       }
 
